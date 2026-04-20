35cb70dd1d8334a4d9d4b5c7d5fc29102a041d12
Alexkarneff <alexkarneff@gmail.com>
2026-04-20 13:54:47 +0200

    Workshop OpenCV

--- Workshops/OpenCV/YoutubeTuto/chapter2.py
+++ Workshops/OpenCV/YoutubeTuto/chapter2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("resources/lena.png")
+img = cv2.imread("../resources/lena.png")
 kernel = np.ones((4,4), np.uint8)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
